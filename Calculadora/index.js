3aa0d41ed8a4c7e45704b80ab8066aae7cfbb143
Latanks <76797064+LAtanks@users.noreply.github.com>
2023-02-25 18:47:56 -0300

    New Calculator

--- Calculadora/index.js
+++ Calculadora/index.js
@@ -1,13 +1,22 @@
 let res = window.document.getElementById("resultado");
 
-function clean(){
-    res.innerHTML = 0
-}
-function back(){
-
-}
 function insert(num){
     
     var numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
 }
+
+function clean(){
+    res.innerHTML = ''
+}
+function back(){
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero.substring(0, numero.length -1);
+}
+function cal(){
+    var res = document.getElementById('resultado').innerHTML;
+    if(res){
+        document.getElementById('resultado').innerHTML = eval(res)
+    }
+}
+
